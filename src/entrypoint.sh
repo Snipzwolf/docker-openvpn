35e2ae44a92ec6c3549997e018d3e81c4eb001b1
John <Snipzwolf@users.noreply.github.com>
2018-01-02 19:22:28 +0000

    added specifying uid and gid via environment vars fixed error due to /dev/net/tun not existing

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 EXTRA_SCRIPT="";
+
 CREATE_CERTS="false";
 CLIENT_CERTS=0;
-SERVER_CONFIG="/etc/openvpn/server.conf";
-EASYRSA_CONFIG="/etc/openvpn/easy-rsa-vars.example";
-
 SERVER_CN="server"
 CLIENT_CN_TEMPLATE="client_@@";
+SERVER_CONFIG="/etc/openvpn/server.conf";
+EASYRSA_CONFIG="/etc/openvpn/easy-rsa-vars.example";
 
 while getopts e:c:p:si:o: option
 do
@@ -30,4 +30,15 @@ if [ $CREATE_CERTS = "true" ] && [ ! -f /etc/openvpn/server/certs_setup ]; then
   exec -c /bin/bash /opt/cert_setup.sh "$SERVER_CN" "$CLIENT_CN_TEMPLATE" $CLIENT_CERTS "$EASYRSA_CONFIG" < /dev/null;
 fi
 
-#exec /usr/sbin/openvpn --config $SERVER_CONFIG < /dev/null
+OPENVPN_USERNAME=${OPENVPN_USERNAME:-"openvpn"};
+OPENVPN_UID=${OPENVPN_UID:-500};
+OPENVPN_GROUPNAME=${OPENVPN_GROUPNAME:-"openvpn"};
+OPENVPN_GID=${OPENVPN_GID:-500};
+id -g $OPENVPN_GROUPNAME &>/dev/null || (groupadd -r $OPENVPN_GROUPNAME -g $OPENVPN_GID && echo "Created group $OPENVPN_GROUPNAME:$OPENVPN_GID");
+id -u $OPENVPN_USERNAME &>/dev/null || (useradd $OPENVPN_USERNAME -rMN -u $OPENVPN_UID -g $OPENVPN_GID && echo "Created user $OPENVPN_USERNAME:$OPENVPN_UID");
+
+
+[ -d /dev/net ] || mkdir -p /dev/net
+[ -c /dev/net/tun ] || (mknod /dev/net/tun c 10 200 && chmod 0666 /dev/net/tun )
+
+exec /usr/sbin/openvpn --config $SERVER_CONFIG < /dev/null
